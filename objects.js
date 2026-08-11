393d069e56e7a52410e1a31ece33aa128d391956
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-08-11 19:14:12 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+const student = {
+    name: "Bhoomika",
+    age: 21,
+    course: "AI&DS",
+    college: "DBIT"
+};
+console.log(student.name);
+
+const product = {
+    name: "Laptop",
+    price: 60000,
+    brand: "Dell"
+};
+product.price = 55000;
+console.log(product.price);
